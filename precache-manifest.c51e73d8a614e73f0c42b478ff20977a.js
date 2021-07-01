f70def47c4a44e1ec58c3a5c6dfa9f9b4e0ebfce
jefmagic <jwmckenn@umich.edu>
2021-06-30 23:11:19 -0400

    Updates

---
+++ precache-manifest.c51e73d8a614e73f0c42b478ff20977a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea4bbc087983d0b1dd1b",
+    "url": "/static/js/app.0fa89da2.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "5bb4d218a18fff83bec6",
+    "url": "/static/js/2.9b682d1b.chunk.js"
+  },
+  {
+    "revision": "c6ecb5460849d80bb9513ebb43df799e",
+    "url": "/toast.PNG"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b682d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "30f8e4c7403f1609d8d968333af0e087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0431890deaadc6d6c93ea915167d0c9",
+    "url": "/manifest.json"
+  }
+];
